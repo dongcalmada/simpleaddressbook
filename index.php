8b47b8b052ce41684e6f3dac88a360d664aa6b92
Dong Calmada <dbcalmada@up.edu.ph>
2019-08-18 11:30:33 +0800

    main_table_def update

--- index.php
+++ index.php
@@ -1,29 +1,16 @@
 <?php
 session_start();
-include '.config.php';
+require '.config.php';
 include 'utilities.php';
+
 $mode = 'home';
+
 if (isset($_GET['mode'])) {
 	$mode = $_GET['mode'];
 }
 
 echo html_top();
 echo html_banner();
-switch ($mode) {
-	case 'home':
-		echo "home page here!";
-		break;
-	case 'add':
-		echo "add contact form here!";
-		break;
-	case 'edit':
-		echo "edit contact form here!";
-		break;
-	case 'delete':
-		echo "delete contact form here!";
-		break;
-	case 'view':
-		echo "view contact form here!";
-		break;
-}
+
+
 echo html_bottom();
