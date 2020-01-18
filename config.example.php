0979a91dbea02ed15d9eea016f715295cb4029e8
Dong Calmada <dungkal@dalisay.upm.edu.ph>
2020-01-18 12:49:49 +0800

    New file: config.example.php

---
+++ config.example.php
@@ -0,0 +1,15 @@
+<?php
+define('SITE_URL','http://localhost');
+define('SITE_NAME','My Application');
+define('DB','');
+define('DB_HOST','localhost');
+define('DB_USER','');
+define('DB_PASS','');
+define('MAIN_TABLE',''); // From the database
+define('MAIN_TABLE_DEFAULT_ORDER_FIELD','');
+define('RECORD_NAME','');
+define('AUTHOR', '');
+define('COPYRIGHT',"Copyright &copy; " . date('Y') . " " . AUTHOR);
+define('ROWS_LIMIT',100);
+define('ADMIN','');
+define('ADMIN_PASS',''); // use SHA1
